85e33c96046273feda450c8110e30272bc803f7c
swillrich5 <swillrich@gmail.com>
2021-05-13 15:23:04 -0500

    fix to schema - exercise name unique: false

--- models/workout.js
+++ models/workout.js
@@ -14,7 +14,7 @@ const WorkoutSchema = new Schema({
             },
             name: {
               type: String,
-              unique: true
+              unique: false
             },
             duration: {
               type: Number,
